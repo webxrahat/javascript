aeb59ccdfe61123bc418c773e3c93f3913c8a780
Md Rahat Islam <islammdrahat308@gmail.com>
2025-03-17 22:13:30 +0600

    #2

--- basic/main.js
+++ basic/main.js
@@ -1,5 +1,26 @@
-const problem = () => {
- return console.log("hello world");
-};
+// Create an object representing a book with properties like title, author, and pages. Write a function that returns the title of the book.
+// const book = {
+//  title: "the world adventure",
+//  author: "Petter coster",
+//  pages: 552,
+// };
 
-problem();
+// function bookName() {
+//  return book.title;
+// }
+
+// console.log(bookName());
+
+// 2. Create a car object with properties brand, model, and year. Write a function that checks if the car is older than 10 years.
+
+// const car = {
+//  brand: "tata",
+//  model: "nano",
+//  year: 2001,
+// };
+// function carYear(carObj) {
+//  const calculateYear = new Date().getFullYear();
+//  return calculateYear - carObj.year > 10;
+// }
+
+// console.log(carYear(car));
