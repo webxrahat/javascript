ddb6eeb24e553bfeb2e23c306468798d833a8e8c
Md Rahat Islam <islammdrahat308@gmail.com>
2025-03-18 22:06:07 +0600

    generte RGB color

--- project1/main.js
+++ project1/main.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+window.onload = () => {
+ main();
+};
+
+function main() {
+ const color = document.getElementById("root");
+ const btn = document.getElementById("btn");
+
+ btn.addEventListener("click", function () {
+  color.style.backgroundColor = generateColor();
+ });
+}
+
+function generateColor() {
+ const red = Math.floor(Math.random() * 255);
+ const blue = Math.floor(Math.random() * 255);
+ const green = Math.floor(Math.random() * 255);
+ return `rgb(${red}, ${green}, ${blue})`;
+}
