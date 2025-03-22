79d8ad35d148fffefa1676cae925822ab83ed540
Md Rahat Islam <islammdrahat308@gmail.com>
2025-03-22 22:33:37 +0600

    find largesrt city

---
+++ problems/main.js
@@ -0,0 +1,24 @@
+// Create an object with keys as city names and values as their populations. Write a function that finds and returns the city with the largest population.
+
+const obj = {
+ Dhaka: 1000,
+ jashore: 1200,
+ khulna: 1500,
+ bogura: 1100,
+};
+
+function largestPopulation(objects) {
+ let maxCity = "";
+ let maxPopulation = 0;
+ for (let x in objects) {
+  if (objects[x] > maxPopulation) {
+   maxPopulation = objects[x];
+   console.log(maxPopulation);
+   maxCity = x;
+  }
+ }
+ return maxCity;
+}
+
+const ans = largestPopulation(obj);
+// console.log(ans);
